0bd698ff46c1cb04619d944fad14d892fe860664
Marcos Ledezma <m.ledezma@esencia.com.py>
2013-04-02 11:19:41 -0300

    Maquetacion

--- addContact.php
+++ addContact.php
@@ -3,7 +3,7 @@
 include("usuarios.php");
 
 mysql_query("INSERT INTO contacto (FirstName, LastName, Mail)
-VALUES ('$_POST[FirstName]', '$_POST[LastName]', '$_POST[Mail]')")
+VALUES ('$_POST[FirstName]', '$_POST[LastName]', '$_POST[Mail]', '$_POST[Code]')")
 	or die (mysql_error());
 
 echo "Nuevo contacto agregado";
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+<?/*php
 
 include("usuarios.php");
 mysql_query("INSERT INTO contacto (FirstName, LastName, Mail)
@@ -7,4 +7,46 @@ VALUES ('Marcos', 'Ledezma', 'm.ledezma@esencia.com.py')")
 	
 echo "Nuevo contacto agregado";
 
-?>
+*/?>
+<html>
+
+<head>
+<link rel="stylesheet" type="text/css" media="screen" href="styles.css" />
+</head>
+
+<body>
+
+	<div class="content">
+		<div class="h1container">
+			<h1>Administracion de Usuarios</h1>
+		</div>
+		<form action="addContact.php" method="post">
+			
+			<div class="inputContent">
+				<label class="title" for="name">Nombre:</label>
+				<input type="text" name="FirstName" id="name">
+			</div>
+			
+			<div class="inputContent">
+				<label class="title" for="lastname">Apellido:</label>
+				<input type="text" name="LastName" id="lastname">
+			</div>
+			
+			<div class="inputContent">
+				<label class="title" for="mail">Mail:</label>
+				<input type="text" name="Mail" id="mail">
+			</div>
+			
+			<div class="inputContent">
+				<label class="title" for="Code">Codigo:</label>
+				<input type="text" name="Code" id="Code">
+			</div>
+			
+			<input class="send" type="submit" value="Agregar">
+
+
+		</form>
+	</div>
+
+</body>
+</html>
